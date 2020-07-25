2076a10478a38d006a346f786cc4801e032cd9a8
Jason Kang <min.liang.kang@jpl.nasa.gov>
2020-07-25 15:21:12 -0700

    Refactor longest_subarray.rb

--- Longest Sub Array/Ruby/longest_subarray.rb
+++ Longest Sub Array/Ruby/longest_subarray.rb
@@ -11,31 +11,31 @@ def is_even(val)
 end
 
 input = Array.[](5, 2, 4, 7, 6, 18, 14, 0)
-len = Array.new(input.length)
-longest_sub_array_index = nil
-range = (input.length-1)..0
-(range.first).downto(range.last).each do |i|
-    if i+1 >= len.length
+accumulate_sub_array_len = Array.new(input.length)
+longest_sub_array_end_index = nil
+
+(0..(input.length-1)).each do |i|
+    if i == 0
         if is_even(input[i])
-            len[i] = 1
-            longest_sub_array_index = i
+            accumulate_sub_array_len[i] = 1
+            longest_sub_array_end_index = i
         else
-            len[i] = 0
+            accumulate_sub_array_len[i] = 0
         end
     else
         if is_even(input[i])
-            len[i] = len[i+1] > 0 ? len[i+1]+1 : 1
-            if longest_sub_array_index
-                longest_sub_array_index = len[i] >= len[longest_sub_array_index] ? i : longest_sub_array_index
+            accumulate_sub_array_len[i] = accumulate_sub_array_len[i-1] > 0 ? accumulate_sub_array_len[i-1]+1 : 1
+            if longest_sub_array_end_index
+                longest_sub_array_end_index = accumulate_sub_array_len[i] >= accumulate_sub_array_len[longest_sub_array_end_index] ? i : longest_sub_array_end_index
             else
-                longest_sub_array_index = i
+                longest_sub_array_end_index = i
             end
         else
-            len[i] = 0
+            accumulate_sub_array_len[i] = 0
         end
     end
 end
 
-puts "Length = #{len[longest_sub_array_index]}"
-puts "subArr[] = #{input.slice(longest_sub_array_index, len[longest_sub_array_index])}"
+puts "Length = #{accumulate_sub_array_len[longest_sub_array_end_index]}"
+puts "subArr[] = #{input.slice(longest_sub_array_end_index-accumulate_sub_array_len[longest_sub_array_end_index]+1, accumulate_sub_array_len[longest_sub_array_end_index])}"
 
